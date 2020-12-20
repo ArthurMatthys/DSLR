04b4a4de614b3053638308b4a6ab2d4a270e9eab
Guillaume Rival de rouville <gurival-@z3r12p3.42lyon.fr>
2020-12-20 18:21:47 +0100

    clean histogram, just need to implement visualization

--- Data_Visualization/histogram.py
+++ Data_Visualization/histogram.py
@@ -1,48 +1,44 @@
 import os
 import sys
+from matplotlib import pyplot
 
 sys.path.insert(0,
                 os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import lib
+
+def sephist(col):
 
 
 def main():
     """
     Main function
     """
-    names = [('Hogwarts House', 2), ('Arithmancy', 1), ('Astronomy', 1),
-             ('Herbology', 1), ('Defense Against the Dark Arts', 1),
-             ('Divination', 1), ('Muggle Studies', 1), ('Ancient Runes', 1),
-             ('History of Magic', 1), ('Transfiguration', 1), ('Potions', 1),
-             ('Care of Magical Creatures', 1), ('Charms', 1), ('Flying', 1)]
-
+    names = ["Index","Hogwarts House","First Name","Last Name","Birthday",
+            "Best Hand","Arithmancy","Astronomy","Herbology",
+            "Defense Against the Dark Arts","Divination",
+            "Muggle Studies","Ancient Runes","History of Magic",
+            "Transfiguration","Potions","Care of Magical Creatures",
+            "Charms","Flying"]
     hogwarts_house = ["Hufflepuff", "Gryffindor", "Slytherin", "Ravenclaw"]
+
     args = lib.get_args()
-    data = lib.open_data(args.data)
-    data = [e.split(',') for e in data.split('\n')][:-1]
-    column_type = lib.find_column(data[0], names)
-    dic = {"Hufflepuff": [], "Gryffindor": [],
-           "Slytherin": [], "Ravenclaw": []}
-    name = column_type.index(2)
-    for line in data[1:]:
-        dic[line[name]].append(line)
+    df = lib.open_data(args.data, names)[1:]
+    df = df.drop(columns=["Index","First Name","Last Name","Birthday","Best Hand"])
+    df = df.dropna()
+    cols = df.columns
+    df[cols[1:]] = df[cols[1:]].astype(float)
 
-    header = []
-    for i, name in enumerate(data[0]):
-        if column_type[i] == 1:
-            header.append([name])
-    print(header)
-    all_data = []
-    for house in hogwarts_house:
-        actual_data = dic[house]
-        new_format = header
-        index = 0
-        for _, line in enumerate(actual_data):
-            for i, feature in enumerate(line):
-                if column_type[i] == 1:
-                    new_format[index].append(float(feature))
-                    index += 1
+    list_colnames = list(cols)[1:]
+    for 
 
+    # print(list_colnames)   
+    # print(df) 
+    # for name in list_colnames:
+    #     subset = df[name].hist(by=df['Hogwarts House'])
+    #     for axis in subset.flatten():
+    #         axis.set_xticklabels([])
+    #         axis.set_yticklabels([])
+    # pyplot.show()
 
 if __name__ == "__main__":
     main()
